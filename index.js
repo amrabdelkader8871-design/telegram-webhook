0551e07a06db4d0a667389f6201d356cdde396d5
amrabdelkader8871-design <amr.abdelkader8871@gmail.com>
2025-12-18 11:40:54 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,16 @@
+const express = require("express");
+
+const app = express();
+app.use(express.json());
+
 app.post("/telegram-webhook", (req, res) => {
   console.log("Telegram Update:", JSON.stringify(req.body));
   res.sendStatus(200);
 });
+
+app.get("/", (req, res) => {
+  res.send("Telegram bot is running ✅");
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log("Running on port", PORT));
